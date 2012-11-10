83fbc4b2752a000025d9a3617dc6f6128e659d2f
Blithe Rocher <blitherocher@gmail.com>
2012-11-10 16:15:02 -0500

    Add more exercises

---
+++ ex13.rb
@@ -0,0 +1,18 @@
+first, second, third = ARGV 
+
+print "Fourth variable? "
+fourth = STDIN.gets.chomp()
+print "Fifth variable? " 
+fifth = STDIN.gets.chomp()
+print "Sixth variable? " 
+sixth = STDIN.gets.chomp() 
+
+
+
+puts "The script is called: #{$0}"
+puts "Your first variable is #{first}"
+puts "Your second variable is #{second}"
+puts "Your third variable is #{third}"
+puts "Your fourth variable is #{fourth}"
+puts "Your fifth variable is #{fifth}"
+puts "Your sixth variable is #{sixth}"
---
+++ form.rb
@@ -0,0 +1,8 @@
+print "What is your name? "
+name = gets.chomp()
+print "Where are you from? "
+place = gets.chomp()
+print "What is your favorite color? "
+color = gets.chomp()
+
+puts "So, you're #{name}, you're from #{place} and your favorite color is #{color}."
